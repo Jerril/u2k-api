59502465901198fb7965bb59f559d49f78a4d133
Jeremiah Ekundayo <dayoolapeju@outlook.com>
2023-06-04 00:24:04 +0100

    update paystack service

--- app/Services/Paystack.php
+++ app/Services/Paystack.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Http;
 
 class Paystack
 {
+    /*
+        WITHDRAWAL TRANSACTION - FROM U2K PAYSTACK WALLET TO BANK ACCOUNT (SENDING MONEY TO BANK ACCOUNT FROM PAYSTACK WALLET)
+    */
+
     // get NG banks
     public function listBanks()
     {
@@ -17,11 +21,60 @@ class Paystack
     }
 
     // verify bank details
+    public function verifyAccountDetails($account_no, $bank_code)
+    {
+        $response = Http::withHeaders([
+            "Authorization" => "Bearer ".env('PAYSTACK_SECRET_KEY'),
+            "Cache-Control" => 'no-cache'
+        ])->get("https://api.paystack.co/bank/resolve?account_number=$account_no&bank_code=$bank_code")->json();
+
+        return $response;
+    }
 
     // create transfer recipient
+    public function createTransferRecipient($name, $account_no, $bank_code)
+    {
+        $response = Http::withHeaders([
+            "Authorization" => "Bearer ".env('PAYSTACK_SECRET_KEY'),
+            "Cache-Control" => 'no-cache'
+        ])->post("https://api.paystack.co/transferrecipient", [
+            "type" => "nuban",
+            "name" => $name,
+            "account_number" => $account_no,
+            "bank_code" => $bank_code,
+            "currency" => "NGN"
+        ])->json();
+
+        return $response;
+    }
+
+    // initiate transfer
+    public function initiateTransfer($amount, $recipient, $reference="your-unique-reference")
+    {
+        $response = Http::withHeaders([
+            "Authorization" => "Bearer ".env('PAYSTACK_SECRET_KEY'),
+            "Cache-Control" => 'no-cache'
+        ])->post("https://api.paystack.co/transfer", [
+            "source" => "balance", 
+            "amount" => $amount,
+            "reference" => $reference,
+            "recipient" => $recipient,
+            "reason" => "Holiday Flexing"
+        ])->json();
+
+        return $response;
+    }
+
+    // verify successful transfer/withdrawal using webhook
+
+    
+    
+    /*
+        DEPOSIT TRANSACTION - FROM BANK ACCOUNT THRU CARD PAYMENT INTO U2K PAYSTACK WALLET
+    */
 
     // initialize transfer & get paystack payment modal
-    public function initializeTransfer($email, $amount=10500, $currency="NGN")
+    public function initializeTransfer($email, $amount)
     {
         $response = Http::withHeaders([
             "Authorization" => "Bearer ".env('PAYSTACK_SECRET_KEY'),
@@ -30,7 +83,7 @@ class Paystack
             "email" => $email,
             "amount" => $amount * 100,
             "callback_url" => env('PAYSTACK_CALLBACK_URL'),
-            "currency" => $currency
+            "currency" => "NGN"
         ])->json();
 
         return $response;
